62f09cc0dca794983f26edbc55f469b87e4c2a60
Arif Khan <khanarif2738@gmail.com>
2024-08-24 03:48:35 +0530

    Here the Complete code && Using GPTSearch  is Paid Service So, If we Paid then we can use GPTAPI.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,10 @@
 
-
+// here the TMDB API secret_key which we got from "TMDB" wesite. It's Open Source free Available. 
 export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NDM2N2VkODJlNjE1NDM3M2Q0ZjVlNzVmYzg0NmY5OCIsIm5iZiI6MTcyNDE0Nzc3My44OTU0NDcsInN1YiI6IjY2YzM5ZTZlNGRmMjk0NTZiYWQwZTVhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7j_kE34lz3kAiazMfo-ZGvdffDobMcfNMnWQzAJNs8s'
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NDM2N2VkODJlNjE1NDM3M2Q0ZjVlNzVmYzg0NmY5OCIsIm5iZiI6MTcyNDE0Nzc3My44OTU0NDcsInN1YiI6IjY2YzM5ZTZlNGRmMjk0NTZiYWQwZTVhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7j_kE34lz3kAiazMfo-ZGvdffDobMcfNMnWQzAJNs8s'
   },
 };
 
@@ -21,5 +20,9 @@ export const SUPPORTED_LANGUAGES = [
   {identifire: "spanish", name: "Spanish"},
 ];
 
-//  here the openai key which we got from the "openai.com" wesite 
-export const OPENAI_KEY = "sk-rzRMYc1OuYvVpHFrEkCCa9UTvTGotNJc2BYO6X8qxIT3BlbkFJcgRu9xl51jXmfASSb9oF2XvVL50GOj7xigK0x0iS8A";
+// we provide it security using ".env" file for prevent from the Hackers. This is not show on my gihub code. because .gitignore file is not visible there. 
+// here the openai secret_key which we got from the "openai.com" website. It's Not Open Source we have to pay for this.
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+
+
+
