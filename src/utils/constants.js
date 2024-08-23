2b2d0e970884868181855995a3a5be43920edaea
Arif Khan <khanarif2738@gmail.com>
2024-08-24 04:13:49 +0530

    Here the Complete code && Using GPTSearch  is Paid Service So, If we Paid then we can use GPTAPI.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NDM2N2VkODJlNjE1NDM3M2Q0ZjVlNzVmYzg0NmY5OCIsIm5iZiI6MTcyNDE0Nzc3My44OTU0NDcsInN1YiI6IjY2YzM5ZTZlNGRmMjk0NTZiYWQwZTVhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7j_kE34lz3kAiazMfo-ZGvdffDobMcfNMnWQzAJNs8s'
+    Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_API_KEY,
   },
 };
 
