e3fcac2aad68c4f12b5b305135a6e0b53acbaa26
Arif Khan <khanarif2738@gmail.com>
2024-09-03 00:27:32 +0530

    change

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,10 +1,10 @@
-import OpenAI from 'openai';
+// import OpenAI from 'openai';
 
-const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI_KEY,    // default to process.env ["OPENAI_API_KEY"]
-  dangerouslyAllowBrowser: true    // -> Bypass browser environment restriction. it's important to run our application.
-                                  // -> but it's not good practice for run the application because my "secret key" shown in my code directly.
-});                               // -> it only use for Runtime environment.
+// const openai = new OpenAI({
+//   apiKey: process.env.REACT_APP_OPENAI_KEY,    // default to process.env ["OPENAI_API_KEY"]
+//   dangerouslyAllowBrowser: true    // -> Bypass browser environment restriction. it's important to run our application.
+//                                   // -> but it's not good practice for run the application because my "secret key" shown in my code directly.
+// });                               // -> it only use for Runtime environment.
 
 
-export default openai;
+// export default openai;
